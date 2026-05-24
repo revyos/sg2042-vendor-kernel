zhaohui-yan/linux-riscv

riscv: Reduce competition on the lr instruction for sg2042 128 cores
Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

arch/riscv/include/asm/futex.h
@@ -12,6 +12,7 @@
 #include <linux/errno.h>
 #include <asm/asm.h>
 #include <asm/asm-extable.h>
+#include <asm/cmpxchg.h>
 
 /* We don't even really need the extable code, but for now keep it simple */
 #ifndef CONFIG_MMU
@@ -84,6 +85,7 @@ futex_atomic_cmpxchg_inatomic(u32 *uval, u32 __user *uaddr,
 		return -EFAULT;
 
 	__enable_user_access();
+	pre_lrsc((unsigned long)uaddr);
 	__asm__ __volatile__ (
 	"1:	lr.w.aqrl %[v],%[u]			\n"
 	"	bne %[v],%z[ov],3f			\n"
@@ -95,6 +97,7 @@ futex_atomic_cmpxchg_inatomic(u32 *uval, u32 __user *uaddr,
 	: [r] "+r" (ret), [v] "=&r" (val), [u] "+m" (*uaddr), [t] "=&r" (tmp)
 	: [ov] "Jr" ((long)(int)oldval), [nv] "Jr" (newval)
 	: "memory");
+	post_lrsc((unsigned long)uaddr);
 	__disable_user_access();
 
 	*uval = val;

arch/riscv/lib/lrsc.c
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifdef CONFIG_SOPHGO_SG2042_MULTI_SOCKETS_LRSC_DELAY
+#include <linux/types.h>
+#include <linux/cache.h>
+#include <linux/export.h>
+#include <asm/lrsc.h>
+
+unsigned short lrsc_addr[ADDR_NUM] ____cacheline_aligned = {0};
+EXPORT_SYMBOL(lrsc_addr);
+#endif

revyos/sg2042-vendor-kernel

arch/riscv/include/asm/futex.h
@@ -12,6 +12,7 @@
 #include <linux/errno.h>
 #include <asm/asm.h>
 #include <asm/asm-extable.h>
+#include <asm/lrsc.h>
 
 /* We don't even really need the extable code, but for now keep it simple */
 #ifndef CONFIG_MMU
@@ -84,6 +85,7 @@ futex_atomic_cmpxchg_inatomic(u32 *uval, u32 __user *uaddr,
 		return -EFAULT;
 
 	__enable_user_access();
+	pre_lrsc((unsigned long)uaddr);
 	__asm__ __volatile__ (
 	"1:	lr.w.aqrl %[v],%[u]			\n"
 	"	bne %[v],%z[ov],3f			\n"
@@ -95,6 +97,7 @@ futex_atomic_cmpxchg_inatomic(u32 *uval, u32 __user *uaddr,
 	: [r] "+r" (ret), [v] "=&r" (val), [u] "+m" (*uaddr), [t] "=&r" (tmp)
 	: [ov] "Jr" ((long)(int)oldval), [nv] "Jr" (newval)
 	: "memory");
+	post_lrsc((unsigned long)uaddr);
 	__disable_user_access();
 
 	*uval = val;

arch/riscv/lib/lrsc.c
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifdef CONFIG_SOPHGO_SG2042_MULTI_SOCKETS_LRSC_DELAY
+#include <linux/types.h>
+#include <linux/cache.h>
+#include <linux/export.h>
+#include <asm/lrsc.h>
+
+unsigned short lrsc_count[ADDR_NUM] ____cacheline_aligned = {0};
+EXPORT_SYMBOL(lrsc_count);
+#endif